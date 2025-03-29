d956b9fdf64235b3be4ff3ade215308f5d337499
yudystriawan <yudistriawan@gmail.com>
2025-03-29 21:48:39 +0800

    Remove display property from Poppins font configuration in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-poppins",
-  display: "swap",
   style: ["normal", "italic"],
 });
 
