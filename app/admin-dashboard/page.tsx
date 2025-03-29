1bebbdc9b0eea1f778e9de6dd8faad78220a3c18
yudystriawan <yudistriawan@gmail.com>
2025-03-29 22:15:55 +0800

    Update AdminDashboardPage to handle pagination with search parameters in PropertiesTable

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,10 +1,18 @@
 import BreadCrumbs from "@/components/breadcrumbs";
 import { Button } from "@/components/ui/button";
 import { PlusCircleIcon } from "lucide-react";
+import { SearchParams } from "next/dist/server/request/search-params";
 import Link from "next/link";
 import PropertiesTable from "./_components/properties-table";
 
-const AdminDashboardPage = async () => {
+const AdminDashboardPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) => {
+  const { page }: { page?: string } = await searchParams;
+
+  console.log(page);
   return (
     <div>
       <BreadCrumbs
@@ -20,7 +28,7 @@ const AdminDashboardPage = async () => {
           <PlusCircleIcon /> New Property
         </Link>
       </Button>
-      <PropertiesTable />
+      <PropertiesTable page={page ? parseInt(page) : 1} />
     </div>
   );
 };
