78e5039606417f526c13db14dbf421d1a86ca6a3
yudystriawan <yudistriawan@gmail.com>
2025-03-29 13:41:16 +0800

    Add Avatar and DropdownMenu components for enhanced user profile interaction

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,23 +1,65 @@
 "use client";
 
 import { useAuth } from "@/context/auth";
+import Image from "next/image";
 import Link from "next/link";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const AuthButton = () => {
   const auth = useAuth();
   return (
     <div>
       {!!auth?.currentUser && (
-        <>
-          <div>{auth.currentUser.email}</div>
-          <div
-            onClick={() => {
-              auth.logout();
-            }}
-          >
-            Logout
-          </div>
-        </>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              {!!auth.currentUser.photoURL && (
+                <Image
+                  src={auth.currentUser.photoURL}
+                  alt={`${auth.currentUser.displayName} avatar`}
+                  width={70}
+                  height={70}
+                />
+              )}
+              <AvatarFallback>
+                {(auth.currentUser.displayName || auth.currentUser.email)?.[0]}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <div>{auth.currentUser.displayName}</div>
+              <div className="font-normal text-xs">
+                {auth.currentUser.email}
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href="/account">My Account</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/admin-dashboard">Admin Dashboard</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/account/my-favourites">My Favourites</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={async () => {
+                await auth.logout();
+              }}
+            >
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
       {!auth?.currentUser && (
         <div className="flex gap-2 items-center">
