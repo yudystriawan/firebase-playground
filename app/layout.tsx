8ebcefc917d89f9620e7f53890bacb7d991fc05e
yudystriawan <yudistriawan@gmail.com>
2025-03-29 23:41:26 +0800

    Remove variable declaration for Poppins font in layout configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import "./globals.css";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
   style: ["normal", "italic"],
   display: "auto",
   adjustFontFallback: true,
