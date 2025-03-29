978d55a87728e4ca35b21ee034655ea8c106ee93
yudystriawan <yudistriawan@gmail.com>
2025-03-29 21:23:35 +0800

    Add error and success notifications to property creation form

--- app/admin-dashboard/new/_components/new-property-form.tsx
+++ app/admin-dashboard/new/_components/new-property-form.tsx
@@ -3,12 +3,16 @@
 import { useAuth } from "@/context/auth";
 import { propertyFormSchema } from "@/validation/propertySchema";
 import { PlusCircleIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { z } from "zod";
 import { createProperty } from "../actions";
 import PropertyForm from "./property-form";
 
 const NewPropertyForm = () => {
   const auth = useAuth();
+  const router = useRouter();
+
   const handleSubmit = async (data: z.infer<typeof propertyFormSchema>) => {
     console.log("Form submitted with data:", data);
 
@@ -23,7 +27,18 @@ const NewPropertyForm = () => {
       token: token,
     });
 
-    console.log("Response from createProperty:", response);
+    if (response.status !== 200) {
+      toast.error("Opps", {
+        description:
+          response.message || "An error occurred while creating the property",
+      });
+      return;
+    }
+
+    toast.success("Success", {
+      description: "Property created successfully",
+    });
+    router.push("/admin-dashboard");
   };
   return (
     <div>
