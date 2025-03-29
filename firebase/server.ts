a58d4daa4fc31774eb1a21b0b8a2e18fb7ec42e8
yudystriawan <yudistriawan@gmail.com>
2025-03-29 15:29:21 +0800

    Refactor Firebase server setup to import service account from JSON file and streamline initialization

--- firebase/server.ts
+++ firebase/server.ts
@@ -1,34 +1,17 @@
-import {
-  cert,
-  getApps,
-  initializeApp,
-  ServiceAccount,
-} from "firebase-admin/app";
+import admin from "firebase-admin";
+import { getApps, ServiceAccount } from "firebase-admin/app";
 import { Auth, getAuth } from "firebase-admin/auth";
 import { Firestore, getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: "playground-29d19",
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40playground-29d19.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com",
-};
+import serviceAccount from "../service-account-key.json";
 
 let firestore: Firestore;
 let auth: Auth;
 const currentApps = getApps();
 
 if (!currentApps.length) {
-  const app = initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
+  const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
   });
   firestore = getFirestore(app);
   auth = getAuth(app);
