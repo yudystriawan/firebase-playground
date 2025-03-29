aa70c7ce4b17fe9453263a7b26aba749c46c8cf5
yudystriawan <yudistriawan@gmail.com>
2025-03-29 21:48:17 +0800

    Add Property type to define property structure with relevant fields

---
+++ types/property.ts
@@ -0,0 +1,14 @@
+import { PropertyStatus } from "./propertyStatus";
+
+export type Property = {
+  id: string;
+  address1: string;
+  address2?: string;
+  city: string;
+  postcode: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  description: string;
+  status: PropertyStatus;
+};
