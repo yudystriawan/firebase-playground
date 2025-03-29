2532bc9f1cdb86f91d322d099d58d9dbb000db0f
yudystriawan <yudistriawan@gmail.com>
2025-03-29 22:15:49 +0800

    Set display property to auto for Poppins font configuration in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-poppins",
   style: ["normal", "italic"],
+  display: "auto",
 });
 
 export const metadata: Metadata = {
