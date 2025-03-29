3cfb4712e010d10078b33d098793bf4bf1e72710
yudystriawan <yudistriawan@gmail.com>
2025-03-29 15:51:17 +0800

    Add middleware for admin dashboard access control

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { decodeJwt } from "jose";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  if (request.method === "POST") return NextResponse.next();
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("firebaseAuthToken")?.value;
+  if (!token) return NextResponse.redirect(new URL("/login", request.url));
+
+  const decodedToken = decodeJwt(token);
+  if (!decodedToken.admin) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin-dashboard"],
+};
