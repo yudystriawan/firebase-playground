43ed7c9b90218685d58af1897cfa619b5c5c17d2
yudystriawan <yudistriawan@gmail.com>
2025-03-29 12:47:42 +0800

    Fix layout rendering by moving children inside AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({
               </li>
             </ul>
           </nav>
+          {children}
         </AuthProvider>
-        {children}
       </body>
     </html>
   );
