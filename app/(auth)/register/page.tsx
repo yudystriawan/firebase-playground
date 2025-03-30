ce1efabd8f625c76c81602679f081b6e13887bc3
yudystriawan <yudistriawan@gmail.com>
2025-03-30 21:53:49 +0800

    Refactor RegisterPage to use Card component for improved layout and styling

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,17 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import RegisterForm from "./_components/register-form";
+
 const RegisterPage = () => {
-  return <div>Register Page</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold">Register</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <RegisterForm />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default RegisterPage;
