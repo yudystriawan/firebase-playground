ec01f4bf161f4a98e958f447fb226ae443e708ae
yudystriawan <yudistriawan@gmail.com>
2025-03-31 10:47:58 +0800

    Fetch user favorites in PropertyCard and conditionally render ToggleFavoriteButton based on authentication

--- app/property-search/_components/property-card.tsx
+++ app/property-search/_components/property-card.tsx
@@ -1,14 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { getUserFavorites } from "@/data/favorites";
+import { auth } from "@/firebase/server";
 import { formatPrice } from "@/lib/price-format";
 import { storageUrlMapper } from "@/lib/storage-url-mapper";
 import { Property } from "@/types/property";
+import { DecodedIdToken } from "firebase-admin/auth";
 import { BathIcon, BedIcon, HomeIcon } from "lucide-react";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 import ToggleFavoriteButton from "./toggle-favorite-button";
 
-const PropertyCard = (props: { property: Property; isFavorite: boolean }) => {
+const PropertyCard = async (props: { property: Property }) => {
   const property = props.property;
 
   const addressLine = [
@@ -20,14 +24,26 @@ const PropertyCard = (props: { property: Property; isFavorite: boolean }) => {
     .filter((addressLine) => !!addressLine)
     .join(", ");
 
+  const favorites = await getUserFavorites();
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("firebaseAuthToken")?.value;
+  let verifiedToken: DecodedIdToken | null = null;
+
+  if (token) {
+    verifiedToken = await auth.verifyIdToken(token);
+  }
+
   return (
     <Card key={property.id} className="py-0 overflow-hidden">
       <CardContent className="px-0">
         <div className="h-40 relative bg-sky-50 text-zinc-400 flex flex-col justify-center items-center">
-          <ToggleFavoriteButton
-            propertyId={property.id}
-            isFavorite={props.isFavorite}
-          />
+          {(!verifiedToken || !verifiedToken.admin) && (
+            <ToggleFavoriteButton
+              propertyId={property.id}
+              isFavorite={favorites[property.id] || false}
+            />
+          )}
           {!!property.images?.[0] && (
             <Image
               fill
