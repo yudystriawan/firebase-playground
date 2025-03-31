b9897a5c8479849b963d1b7d875e5f0fa22adb4f
yudystriawan <yudistriawan@gmail.com>
2025-03-31 10:47:54 +0800

    Implement toggle functionality for favorites in ToggleFavoriteButton with success feedback

--- app/property-search/_components/toggle-favorite-button.tsx
+++ app/property-search/_components/toggle-favorite-button.tsx
@@ -2,26 +2,41 @@
 
 import { useAuth } from "@/context/auth";
 import { HeartIcon } from "lucide-react";
-import { addFavorite } from "../actions";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { addFavorite, removeFavorite } from "../actions";
 
 const ToggleFavoriteButton = (props: {
   propertyId: string;
   isFavorite: boolean;
 }) => {
   const auth = useAuth();
+  const router = useRouter();
 
   const toggleFavorite = async () => {
     const tokenResult = await auth?.currentUser?.getIdTokenResult();
     if (!tokenResult) return;
 
-    await addFavorite(props.propertyId, tokenResult.token);
+    if (props.isFavorite) {
+      await removeFavorite(props.propertyId, tokenResult.token);
+    } else {
+      await addFavorite(props.propertyId, tokenResult.token);
+    }
+
+    toast.success(
+      `Property ${props.isFavorite ? "removed from" : "added to"} favorites`
+    );
+    router.refresh();
   };
   return (
     <button
-      className="absolute top-0 right-0 z-10 p-2"
+      className="absolute top-0 right-0 z-10 p-2 bg-white rounded-bl-lg hover:cursor-pointer"
       onClick={toggleFavorite}
     >
-      <HeartIcon fill={props.isFavorite ? "pink" : "white"} />
+      <HeartIcon
+        fill={props.isFavorite ? "#db2777" : "white"}
+        className="text-black"
+      />
     </button>
   );
 };
