5e2903ddf2f02d5d87efd0e455f09064ddc1a7ae
yudystriawan <yudistriawan@gmail.com>
2025-03-31 10:34:29 +0800

    Enhance ToggleFavoriteButton to support favorite toggling with authentication

--- app/property-search/_components/toggle-favorite-button.tsx
+++ app/property-search/_components/toggle-favorite-button.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { useAuth } from "@/context/auth";
 import { HeartIcon } from "lucide-react";
+import { addFavorite } from "../actions";
 
-const ToggleFavoriteButton = () => {
+const ToggleFavoriteButton = (props: {
+  propertyId: string;
+  isFavorite: boolean;
+}) => {
+  const auth = useAuth();
+
+  const toggleFavorite = async () => {
+    const tokenResult = await auth?.currentUser?.getIdTokenResult();
+    if (!tokenResult) return;
+
+    await addFavorite(props.propertyId, tokenResult.token);
+  };
   return (
-    <button className="absolute top-0 right-0 z-10 p-2">
-      <HeartIcon />
+    <button
+      className="absolute top-0 right-0 z-10 p-2"
+      onClick={toggleFavorite}
+    >
+      <HeartIcon fill={props.isFavorite ? "pink" : "white"} />
     </button>
   );
 };
